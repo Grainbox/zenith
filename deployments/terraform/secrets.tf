ffc84f95c187082f22cc80f35e4ea13f144bf52e
Grainbox <tagemania@gmail.com>
2026-03-16 17:35:23 +0100

    feat: using github secrets for variables

--- deployments/terraform/secrets.tf
+++ deployments/terraform/secrets.tf
@@ -1,15 +1,30 @@
 locals {
   secrets = ["DATABASE_URL", "API_KEY_SALT", "SLACK_WEBHOOK_URL"]
+
+  secret_values = {
+    "DATABASE_URL"     = var.database_url
+    "API_KEY_SALT"     = var.api_key_salt
+    "SLACK_WEBHOOK_URL" = var.slack_webhook_url
+  }
 }
 
-# Reference existing secrets (created manually via gcloud)
-data "google_secret_manager_secret" "zenith_secrets" {
-  for_each = toset(local.secrets)
+resource "google_secret_manager_secret" "zenith_secrets" {
+  for_each  = toset(local.secrets)
   secret_id = "zenith-${lower(replace(each.value, "_", "-"))}-${var.environment}"
+
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "zenith_secrets" {
+  for_each    = { for k, v in local.secret_values : k => v if v != "" }
+  secret      = google_secret_manager_secret.zenith_secrets[each.key].id
+  secret_data = each.value
 }
 
 resource "google_secret_manager_secret_iam_member" "zenith_runner_access" {
-  for_each  = data.google_secret_manager_secret.zenith_secrets
+  for_each  = google_secret_manager_secret.zenith_secrets
   secret_id = each.value.id
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${google_service_account.zenith_runner.email}"
